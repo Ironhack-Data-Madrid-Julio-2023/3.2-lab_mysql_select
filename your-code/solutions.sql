d074becaf17556ea04669830f1fec32aaa3732a3
chiinwy <chiinwy@gmail.com>
2023-07-26 19:07:07 +0200

    CHallenge 2

--- your-code/solutions.sql
+++ your-code/solutions.sql
@@ -5,10 +5,27 @@ authors.au_id as 'AUTHOR ID',
 authors.au_lname as 'LAST NAME',
 authors.au_fname as 'FIRST NAME',
 titles.title as 'TITLE',
-publishers.pub_name as 'PUBLISHER '
+publishers.pub_name as 'PUBLISHER'
 
 from authors
 
-join titleauthor on titleauthor.au_id=authors.au_id
-join titles on titles.title_id = titleauthor.title_id
-join publishers on publishers.pub_id = titles.pub_id;
+join titleauthor on authors.au_id = titleauthor.au_id
+join titles on titleauthor.title_id = titles.title_id
+join publishers on titles.pub_id = publishers.pub_id;
+
+-- Challenge 2
+
+SELECT authors.au_id AS 'AUTHOR ID',
+       authors.au_lname AS 'LAST NAME',
+       authors.au_fname AS 'FIRST NAME',
+       publishers.pub_name AS 'PUBLISHER',
+       count(titles.title_id) 'TITLE COUNT'
+       
+FROM authors
+LEFT JOIN titleauthor ON authors.au_id = titleauthor.au_id
+LEFT JOIN titles ON titleauthor.title_id = titles.title_id
+LEFT JOIN publishers ON titles.pub_id = publishers.pub_id
+
+GROUP BY authors.au_id, publishers.pub_name;
+
+-- Challenge 3
