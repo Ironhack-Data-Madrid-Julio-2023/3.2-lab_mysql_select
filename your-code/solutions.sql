114bfb42187162080ed7c219b314a4fe019f931f
chiinwy <chiinwy@gmail.com>
2023-07-26 19:38:37 +0200

    2,5

--- your-code/solutions.sql
+++ your-code/solutions.sql
@@ -20,12 +20,19 @@ SELECT authors.au_id AS 'AUTHOR ID',
        authors.au_fname AS 'FIRST NAME',
        publishers.pub_name AS 'PUBLISHER',
        count(titles.title_id) 'TITLE COUNT'
+     
        
 FROM authors
-LEFT JOIN titleauthor ON authors.au_id = titleauthor.au_id
-LEFT JOIN titles ON titleauthor.title_id = titles.title_id
-LEFT JOIN publishers ON titles.pub_id = publishers.pub_id
+JOIN titleauthor ON authors.au_id = titleauthor.au_id
+JOIN titles ON titleauthor.title_id = titles.title_id
+JOIN publishers ON titles.pub_id = publishers.pub_id
 
 GROUP BY authors.au_id, publishers.pub_name;
 
 -- Challenge 3
+
+SELECT
+
+authors.au_id AS 'AUTHOR ID',
+authors.au_lname AS 'LAST NAME',
+authors.au_fname AS 'FIRST NAME',
