b42c8e3c40fc7cd87d7d9dfad147cfae574b299c
Marta Moral <mmoralmh@gmail.com>
2023-07-27 18:53:02 +0200

    'mensaje'

--- your-code/challenge 1 mySQL.sql
+++ your-code/challenge 1 mySQL.sql
@@ -1,10 +1,10 @@
-
-SELECT 
+CREATE TEMPORARY TABLE autores
+ SELECT 
   authors.au_id AS `ID DEL AUTOR`,
   authors.au_lname AS `APELLIDO`, 
   authors.au_fname AS `NOMBRE`, 
   publishers.pub_name AS `EDITORIAL`,
-  count(*) AS `NUM LIBROS`
+  count(*) AS `NUMLIBROS`
   
   
 FROM 
@@ -16,3 +16,36 @@ INNER JOIN
 INNER JOIN
   publishers ON titles.pub_id = publishers.pub_id
 group by authors.au_id, authors.au_lname, authors.au_fname, publishers.pub_name;
+ 
+SELECT 
+  `ID DEL AUTOR`, `NOMBRE`, `APELLIDO`
+FROM 
+  autores
+ORDER BY 
+  `NUMLIBROS` DESC
+LIMIT 3
+
+SELECT 
+  autores.`ID DEL AUTOR`,
+  autores.`NOMBRE`,
+  autores.`APELLIDO`,
+  COALESCE(sales_totals.TOTAL, 0) AS `TOTAL`
+FROM 
+  autores
+LEFT JOIN
+  (
+    SELECT 
+      titleauthor.au_id AS `ID DEL AUTOR`,
+      COUNT(sales.qty) AS `TOTAL`
+    FROM 
+      titleauthor
+    LEFT JOIN
+      sales ON titleauthor.title_id = sales.title_id
+    GROUP BY 
+      titleauthor.au_id
+  ) AS sales_totals
+ON 
+  autores.`ID DEL AUTOR` = sales_totals.`ID DEL AUTOR`
+ORDER BY 
+  `TOTAL` DESC;
+ 
